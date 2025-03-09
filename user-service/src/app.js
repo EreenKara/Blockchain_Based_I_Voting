f84bf965f5d10116ede1efdc8551581d651359f6
Ali Eren Yiğit <sad549333@gmail.com>
2025-03-09 16:28:49 +0300

    rateLimit eklendi

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require("./models/associations");
+const rateLimit = require("express-rate-limit");
+
 const bodyParser = require("body-parser");
 const sequelize=require("./config/database");
 const groupRoutes=require("./routes/groupRoutes");
@@ -8,13 +10,23 @@ const userRoutes = require('./routes/userRoutes');
 const electionAccessUsersRoutes=require('./routes/electionAccessUsersRoutes');
 const electionAccessGroupsRoutes=require('./routes/electionAccessGroupsRoutes');  // Kullanıcı yönlendirmelerini import ediyoruz
 require('dotenv').config();  // .env dosyasını yüklemek için
-
-
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  handler: (req, res, next) => {
+      console.warn(`🚨 Rate limit aşıldı! IP: ${req.ip}, URL: ${req.originalUrl}`);
+      res.status(429).json({ message: "Çok fazla istek attınız, lütfen daha sonra tekrar deneyin." });
+  },
+});
+app.use(limiter);
+
+
 app.use(express.json());
 
 app.use(bodyParser.json());
-app.use('/api/users', userRoutes);
+app.use('/api/users', limiter,userRoutes);
 app.use('/api/groups',groupRoutes);
 app.use('/api/userAdresses',userAdressRoute);
 app.use('/api/ElectionAccesUsers',electionAccessUsersRoutes);
@@ -27,3 +39,5 @@ sequelize.sync().then(() => {
     console.log("Database connected!");
     app.listen(5004, () => console.log("User Service running on port 5001"));
   });
+
+  
